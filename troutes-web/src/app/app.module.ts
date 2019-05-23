f26b544fe54bff3826dac67057acb7f5d41af696
Kris8585 <47618122+Kris8585@users.noreply.github.com>
2019-05-23 15:12:51 -0600

    Se agrgan componentes principales al proyecto

--- troutes-web/src/app/app.module.ts
+++ troutes-web/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountComponent } from './components/account/account.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DetailsComponent } from './components/details/details.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginService } from './services/login/login.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    NavbarComponent,
+    FooterComponent,
+    DetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ troutes-web/src/app/types/user.type.ts
@@ -0,0 +1,11 @@
+type UserType = {
+
+    userId:string;
+    name: string;
+    nationality: string;
+    description: string;
+    followed_sites:number[];
+    email: string;
+    role:string;
+
+}
